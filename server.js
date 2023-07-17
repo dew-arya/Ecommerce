44a615027698ef1564664ffb993a9132c363e262
Aryan Dewangan <97282652+dew-arya@users.noreply.github.com>
2023-07-17 11:38:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ import categoryRoures from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-
+import { fileURLToPath } from "url";
 //configuration
 dotenv.config();
 
 //database connection
 connectDB();
 
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // rest object
 const app = express();
 
